882c1ddb6b2e56b74317bc55efd119072025bbc3
Nurlan <115414961+nurlan989@users.noreply.github.com>
2023-11-23 00:49:37 +0300

    Update BattleSelection.cpp
    deleted copy paste

--- BattleSelection.cpp
+++ BattleSelection.cpp
@@ -5,103 +5,25 @@
 #include "CharacterAtributes.h"
 
 
-enum class Characters
+
+void PrintVersus(Characters character1, Characters character2)
 {
-	Knight = 1,
-	Viking = 2,
-	Wizard = 3
-};
+	std::cout << ChoiceCharacter(character1) << " vs " << ChoiceCharacter(character2) << std::endl;
+}
+
 
 void ProgressOfTheGame()
 {
-	if (character1 == static_cast<int>(Characters::Knight) && character2 == static_cast<int>(Characters::Viking))
+	FightCountdown();
+	auto characterOneType = static_cast<Characters>(character1);
+	auto characterTwoType = static_cast<Characters>(character2);
+
+	PrintVersus(characterOneType, characterTwoType);
+
+	CLEAN_SCREEN;
+	while (DealDamageToCharacter(characterOneType, GetDamageCharacter(characterTwoType)) && DealDamageToCharacter(characterTwoType, GetDamageCharacter(characterTwoType)))
 	{
-		FightCountdown();
-		std::cout << "Knight vs Viking" << std::endl;
 		CLEAN_SCREEN;
-		while (DealDamageToKnight(CharacterDamage(60, 100)) && DealDamageToViking(CharacterDamage(20, 50)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Knight) && character2 == static_cast<int>(Characters::Wizard))
-	{
-		FightCountdown();
-		std::cout << "Knight vs Wizard" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToKnight(CharacterDamage(80, 120)) && DealDamageToWizard(CharacterDamage(20, 50)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Viking) && character2 == static_cast<int>(Characters::Knight))
-	{
-		FightCountdown();
-		std::cout << "Viking vs Knight" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToViking(CharacterDamage(50, 90)) && DealDamageToKnight(CharacterDamage(40, 80)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Viking) && character2 == static_cast<int>(Characters::Wizard))
-	{
-		FightCountdown();
-		std::cout << "Viking vs Wizard" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToViking(CharacterDamage(80, 120)) && DealDamageToWizard(CharacterDamage(20, 50)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Wizard) && character2 == static_cast<int>(Characters::Knight))
-	{
-		FightCountdown();
-		std::cout << "Wizard vs Knight" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToWizard(CharacterDamage(60, 100)) && DealDamageToKnight(CharacterDamage(50, 100)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Wizard) && character2 == static_cast<int>(Characters::Viking))
-	{
-		FightCountdown();
-		std::cout << "Wizard vs Viking" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToWizard(CharacterDamage(80, 120)) && DealDamageToViking(CharacterDamage(20, 50)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Knight) && character2 == static_cast<int>(Characters::Knight))
-	{
-		FightCountdown();
-		std::cout << "Knight vs Knight" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToTraitor(CharacterDamage(50, 90)) && DealDamageToKnight(CharacterDamage(30, 70)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Viking) && character2 == static_cast<int>(Characters::Viking))
-	{
-		FightCountdown();
-		std::cout << "Viking vs Viking" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToTraitor(CharacterDamage(50, 90)) && DealDamageToViking(CharacterDamage(30, 70)))
-		{
-			CLEAN_SCREEN;
-		}
-	}
-	if (character1 == static_cast<int>(Characters::Wizard) && character2 == static_cast<int>(Characters::Wizard))
-	{
-		FightCountdown();
-		std::cout << "Wizard vs Wizard" << std::endl;
-		CLEAN_SCREEN;
-		while (DealDamageToTraitor(CharacterDamage(50, 90)) && DealDamageToWizard(CharacterDamage(30, 70)))
-		{
-			CLEAN_SCREEN;
-		}
 	}
 }
+
