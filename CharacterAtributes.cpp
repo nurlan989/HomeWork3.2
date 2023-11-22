e42efac37d06f6802e15888bf5cb9068c67afb3f
Nurlan <115414961+nurlan989@users.noreply.github.com>
2023-11-23 00:52:06 +0300

    Update CharacterAtributes.cpp

--- CharacterAtributes.cpp
+++ CharacterAtributes.cpp
@@ -6,97 +6,91 @@
 #define CELL std::cout << "-------------------------------------" << std::endl;
 #define SWORD std::cout << "Damage --||======>" << damage << std::endl;
 
-bool DealDamageToKnight( int damage )
-{  // The knight has strong armor
+
+std::string ChoiceCharacter(Characters character)
+{
+	switch (character)
+	{
+	case Characters::Knight:
+		return "Knight";
+	case Characters::Viking:
+		return "Viking";
+	case Characters::Wizard:
+		return "Wizard";
+	default:
+		return "";
+		break;
+	}
+}
+
+bool DealDamageToCharacter( Characters characterType, int damage )
+{
 	static int knightHP = CharacterAtributes::hp + (CharacterAtributes::armor + CharacterAtributes::shield);
-	knightHP -= damage;
-	
-	if (knightHP > 0)
+	static int vikingHP = CharacterAtributes::hp + CharacterAtributes::elixirOfLife;
+	static int wizardHP = CharacterAtributes::hp + CharacterAtributes::magicPowder;
+
+
+	switch (characterType)
+	{
+	case Characters::Knight:
+		return DescriptionOfTheBattle(knightHP, damage, characterType);
+		break;
+	case Characters::Viking:
+		return DescriptionOfTheBattle(vikingHP, damage, characterType);
+		break;
+	case Characters::Wizard:
+		
+		if (damage >= 30)
+		{
+			wizardHP += CharacterAtributes::healthPowder;
+		}
+		return DescriptionOfTheBattle(wizardHP, damage, characterType);
+		break;
+	default:
+		return false;
+		break;
+	}
+}
+
+bool DescriptionOfTheBattle(int& characterHP,int damage, Characters characterType)
+{
+	characterHP -= damage;
+
+	if (characterHP > 0)
 	{  
 		CELL;
 		SWORD;
-		std::cout << "Knight's hp left +" << knightHP << std::endl;
+		std::cout << ChoiceCharacter( characterType ) << " left hp " << characterHP << std::endl;
 		CELL;
 		return true;
 	}
 	else 
 	{
-		std::cout << "Knight is dead (R.I.P)" << std::endl;
-		return false;
-	}
-	
-}
-
-bool DealDamageToViking(int damage)
-{  // The Viking has strong health
-	static int vikingHP = (CharacterAtributes::hp + CharacterAtributes::elixirOfLife);
-	vikingHP -= damage;
-
-	if (vikingHP > 0)
-	{
-		CELL;
-		SWORD;
-		std::cout << "Viking's hp left +" << vikingHP << std::endl;
-		CELL;
-		return true;
-	}
-	else 
-	{
-		std::cout << "Viking is dead (R.I.P)" << std::endl;
-		return false;
-	}
-
-}
-
-bool DealDamageToWizard(int damage)
-{  // The Wizard can heal
-	static int wizardHP = (CharacterAtributes::hp + CharacterAtributes::magicPowder);
-	wizardHP -= damage;
-	auto& addHealth = wizardHP;   
-
-	if (damage >= 30)
-	{
-		addHealth += CharacterAtributes::healthPowder;
-	}
-
-	if (wizardHP > 0)
-	{
-		CELL;
-		SWORD;
-		std::cout << "Wizard's hp left +" << wizardHP << std::endl;
-		CELL;
-		return true;
-	}
-	else 
-	{
-		std::cout << "Wizard is dead (R.I.P)" << std::endl;
-		return false;
-	}
-	
-}
-
-bool DealDamageToTraitor(int damage)
-{  
-	static int traitorHP = (CharacterAtributes::hp + 300);
-	traitorHP -= damage;
-
-	if (traitorHP > 0)
-	{
-		CELL;
-		SWORD;
-		std::cout << "Traitor's hp left +" << traitorHP << std::endl;
-		CELL;
-		return true;
-	}
-	else 
-	{
-		std::cout << "Traitor is dead (R.I.P)" << std::endl;
+		std::cout << ChoiceCharacter( characterType ) << " is dead (R.I.P)" << std::endl;
 		return false;
 	}
 }
 
+int GetDamageCharacter(Characters characterType)
+{
+	switch (characterType)
+	{
+	case Characters::Knight:
+		return GetDamageRandom(30, 60);
+		break;
+	case Characters::Viking:
+		return GetDamageRandom(25, 50);
+		break;
+	case Characters::Wizard:
+		return GetDamageRandom(20, 40);
+		break;
+	default:
+		return 0;
+		break;
+	}
+}
 
-short CharacterDamage(int min, int max)
+short GetDamageRandom(int min, int max)
 {
 	double fraction = 1.0 / static_cast<double>(RAND_MAX);
 	return static_cast<short>(rand() * fraction * (max - min) + min);
